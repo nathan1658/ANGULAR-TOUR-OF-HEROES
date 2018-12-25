f40d0876c16c9399c197cd3544be0225ec978d6f
nathan1658 <pooikei165@gmail.com>
2018-12-25 23:53:09 +0800

    Finished https://angular.io/tutorial/toh-pt4

---
+++ src/app/hero.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
+import {Observable, of} from 'rxjs';
+import {MessageService} from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+
+  constructor(private msgService:MessageService) { }
+
+  getHeroes():Observable<Hero[]>
+  {
+    this.msgService.add('HeroService: fetched heroes');
+    return of(HEROES);
+  }
+
+}
